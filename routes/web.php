5671b1d68f5556337f134184430abeb1e48de5c9
Your Name <you@example.com>
2020-08-10 03:45:12 +0000

    controller_submission2

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
 
 Route::get('/', function () {
     return view('welcome');
+    
+//PHP・laravel09課題はここからです。
+Route::get('XXX/AAA/bbb', 'Admin\AAAController@bbb');
+
+Route::group(['prefix' => 'admin'],function (){
+    Route::get('profile/create','Admin/ProfileController@add');
+    Route::get('profile/edit','Admin/ProfileController@edit');
+});
 });
