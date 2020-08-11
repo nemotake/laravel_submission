e57640a3b9a81c58f691bab7dc6ec464cbf8f21d
nemotake <69111121+nemotake@users.noreply.github.com>
2020-08-11 13:12:26 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ Route::get('/', function () {
     return view('welcome');
     
 //PHP・laravel09課題はここからです。
-Route::get('XXX/AAA/bbb', 'Admin\AAAController@bbb');
+Route::get('XXX', 'AAAController@bbb');
 
 Route::group(['prefix' => 'admin'],function (){
     Route::get('profile/create','Admin/ProfileController@add');
     Route::get('profile/edit','Admin/ProfileController@edit');
 });
-});
+
